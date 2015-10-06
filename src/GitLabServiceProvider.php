606dfb256aa9cf39a2d51ca5a93da1776fb35063
Vincent Klaiber <vincentklaiber@gmail.com>
2015-10-06 18:56:03 +0200

    Added extra check before registering publish command

--- src/GitLabServiceProvider.php
+++ src/GitLabServiceProvider.php
@@ -43,7 +43,7 @@ class GitLabServiceProvider extends ServiceProvider
     {
         $source = realpath(__DIR__.'/../config/gitlab.php');
 
-        if (class_exists('Illuminate\Foundation\Application', false)) {
+        if (class_exists('Illuminate\Foundation\Application', false) && $app->runningInConsole()) {
             $this->publishes([$source => config_path('gitlab.php')]);
         } elseif (class_exists('Laravel\Lumen\Application', false)) {
             $app->configure('gitlab');
