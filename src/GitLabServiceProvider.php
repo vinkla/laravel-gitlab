8242d621ed4ad4ecd7e2179fccd58ef955eabab3
Vincent Klaiber <vincentklaiber@gmail.com>
2015-11-13 09:06:50 +0100

    Improved environment specific code

--- src/GitLabServiceProvider.php
+++ src/GitLabServiceProvider.php
@@ -13,7 +13,9 @@ namespace Vinkla\GitLab;
 
 use Gitlab\Client;
 use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Foundation\Application as LaravelApplication;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Lumen\Application as LumenApplication;
 
 /**
  * This is the GitLab service provider class.
@@ -43,9 +45,9 @@ class GitLabServiceProvider extends ServiceProvider
     {
         $source = realpath(__DIR__.'/../config/gitlab.php');
 
-        if (class_exists('Illuminate\Foundation\Application', false) && $app->runningInConsole()) {
+        if ($app instanceof LaravelApplication && $app->runningInConsole()) {
             $this->publishes([$source => config_path('gitlab.php')]);
-        } elseif (class_exists('Laravel\Lumen\Application', false)) {
+        } elseif ($app instanceof LumenApplication) {
             $app->configure('gitlab');
         }
 
